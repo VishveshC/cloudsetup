90097164446f0df0e4cff4950c59acd24cb97e8f
OpenDoor <stepper1947@gmail.com>
2023-07-07 17:03:11 +0530

    Update run.sh

--- run.sh
+++ run.sh
@@ -3,5 +3,6 @@ apt upgrade -y
 apt install -y qbittorrent-nox
 pm2 stop 0
 pm2 delete 0
+curl -o /root/.config/qBittorrent/qBittorrent.conf https://raw.githubusercontent.com/VishveshC/cloudsetup/master/qBittorrent.conf
 pm2 start "qbittorrent-nox"
 pm2 save --force
