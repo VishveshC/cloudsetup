56c2f8a3692684237696a3db940a169e4dd88637
OpenDoor <stepper1947@gmail.com>
2023-07-07 22:26:11 +0530

    Update run.sh

--- run.sh
+++ run.sh
@@ -6,3 +6,4 @@ pm2 delete 0
 curl -o /root/.config/qBittorrent/qBittorrent.conf https://raw.githubusercontent.com/VishveshC/cloudsetup/master/qBittorrent.conf
 pm2 start "qbittorrent-nox"
 pm2 save --force
+curl -s "https://api.telegram.org/bot6374163490:AAEpRNt8_ymZylHd0aBUtSSZgSHOLvbUVes/sendMessage?chat_id=1497506203&text=Setup%20finished.%0AWebsite:%20http://$(curl -s https://api.ipify.org)"
