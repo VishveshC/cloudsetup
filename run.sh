4d007bcf958c169e76df68da2251632b95fac240
OpenDoor <stepper1947@gmail.com>
2023-07-10 15:42:50 +0530

    Fixed IP sending
    Fixed IP sending

--- run.sh
+++ run.sh
@@ -6,4 +6,5 @@ pm2 delete 0
 curl -o /root/.config/qBittorrent/qBittorrent.conf https://raw.githubusercontent.com/VishveshC/cloudsetup/master/qBittorrent.conf
 pm2 start "qbittorrent-nox"
 pm2 save --force
-curl -s "https://api.telegram.org/bot6374163490:AAEpRNt8_ymZylHd0aBUtSSZgSHOLvbUVes/sendMessage?chat_id=1497506203&text=Setup%20finished.%0AWebsite:%20http://$(curl -s https://api.ipify.org)"
+curl -s "https://api.telegram.org/bot6374163490:AAEpRNt8_ymZylHd0aBUtSSZgSHOLvbUVes/sendMessage?chat_id=1497506203&text=Setup%20finished.%0AWebsite:%20http://$(echo "$HOSTNAME" | sed 's/\(-runtimes\).*//')-0.cloudclusters.net"
+
