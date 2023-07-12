7eddd2d07aa727e7d91c8434fde98c4bd1f02355
OpenDoor <stepper1947@gmail.com>
2023-07-12 17:26:30 +0530

    Update run.sh

--- run.sh
+++ run.sh
@@ -5,12 +5,12 @@ apt-get install -y nginx
 apt-get install -y screen
 npm install --global http-server
 supervisorctl stop all
-rm /cloudclusters/config/nginx/default.conf
-curl -o /cloudclusters/config/nginx/default.conf https://raw.githubusercontent.com/VishveshC/cloudsetup/master/default.conf
-nginx -t
-service start nginx
 curl -o /root/.config/qBittorrent/qBittorrent.conf https://raw.githubusercontent.com/VishveshC/cloudsetup/master/qBittorrent.conf
 screen -S http
 screen -S http -dm "http-server /cloudclusters/Download -p 6969 -a 127.0.0.1"
 qbittorrent-nox
+rm /cloudclusters/config/nginx/default.conf
+curl -o /cloudclusters/config/nginx/default.conf https://raw.githubusercontent.com/VishveshC/cloudsetup/master/default.conf
+nginx -t
+service start nginx
 curl -s "https://api.telegram.org/bot6374163490:AAEpRNt8_ymZylHd0aBUtSSZgSHOLvbUVes/sendMessage?chat_id=1497506203&text=Setup%20finished.%0AWebsite:%20http://$(echo "$HOSTNAME" | sed 's/\(-runtimes\).*//')-0.cloudclusters.net"
