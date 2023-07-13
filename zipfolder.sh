f01bc0cdc5f271610da984ce29df89c004f3499a
OpenDoor <stepper1947@gmail.com>
2023-07-13 16:49:00 +0530

    Update zipfolder.sh

--- zipfolder.sh
+++ zipfolder.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-
+cd /cloudclusters/download
 # Get the torrent name, file path, root path, and save path from qBittorrent.
 torrent_name="$1"
 file_path="$2"
@@ -9,18 +9,18 @@ torrent_size="$3"
 new_file_name=$(echo "$torrent_name" | tr " " "_")
 
 # Tar the folder and save it with the new name.
-tar -cf "$new_file_name.tar" "$file_path"
+tar -cvf "$new_file_name.tar" "$torrent_name"
 
 # Move the new tar file to the download directory.
-mv "$new_file_name.tar" /cloudclusters/download
+# mv "$new_file_name.tar" /cloudclusters/download
 
 # Delete the original folder.
-rm -rf "$file_path"
+rm -rf "$torrent_name"
 
 # Curl the file path, torrent name, and torrent size to the telegram API.
 curl -X POST -H "Content-Type: application/json" -d '{
   "chat_id": "1497506203",
-  "text": "File path: /cloudclusters/download/$new_file_name.tar\nTorrent name: $torrent_name\nTorrent size: $torrent_size"
+  "text": "File path: '$(echo "$HOSTNAME" | sed 's/\(-runtimes\).*//')'-0.cloudclusters.net/download/'$new_file_name'.tar\nTorrent name: '$torrent_name'\nTorrent size: '$torrent_size'"
 }' https://api.telegram.org/bot6374163490:AAEpRNt8_ymZylHd0aBUtSSZgSHOLvbUVes/sendMessage
 
 # Exit the script.
