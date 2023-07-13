4ba96f175d55c0be0c6f0ad729e9d9b3bac20bef
OpenDoor <stepper1947@gmail.com>
2023-07-13 15:09:43 +0530

    Update zipfolder.sh

--- zipfolder.sh
+++ zipfolder.sh
@@ -1,11 +1,38 @@
 #!/bin/bash
 
 # Telegram bot token and chat ID
-bot_token="YOUR_BOT_TOKEN"
-chat_id="YOUR_CHAT_ID"
+bot_token="6374163490:AAEpRNt8_ymZylHd0aBUtSSZgSHOLvbUVes"
+chat_id="1497506203"
+
+# Set the download folder path
+download_folder="/cloudclusters/download"
+
+# Get the current directory
+current_dir="%F"
+
+# Extract the base folder name
+base_folder="$(basename "$current_dir")"
+
+# Replace spaces with underscores in the base folder name
+new_base_folder="${base_folder// /_}"
+
+# Create the tar file path
+tar_file_path="$download_folder/$new_base_folder.tar"
+
+# Create a tar archive without compression
+tar -cf "$tar_file_path" -C "$current_dir" .
+
+# Print out the file path
+echo "Tar archive created: $tar_file_path"
+
+# Remove the original directory
+rm -r "$current_dir"
+
+# Print message after deletion
+echo "Original folder deleted: $current_dir"
 
 # Path to the tar file
-tar_file="/path/to/tar_file.tar"
+tar_file="$tar_file_path"
 
 # Extract file name without path
 file_name=$(basename "$tar_file")
@@ -17,10 +44,10 @@ file_size=$(stat -c%s "$tar_file")
 file_size_gb=$(awk -v size="$file_size" 'BEGIN{ printf "%.1f", size/1024/1024/1024 }')
 
 # Category
-category="YOUR_CATEGORY"
+category="%L"
 
 # Generate the automatic download link
-download_link="https://yourwebsite.com/download/$file_name"
+download_link="http://$(echo "$HOSTNAME" | sed 's/\(-runtimes\).*//')-0.cloudclusters.net/download/$file_name"
 
 # Create the message with properties
 message="File Name: $file_name
