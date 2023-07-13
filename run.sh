01a7d7ba80d86b6dc7d878150cca88e827eb08cf
OpenDoor <stepper1947@gmail.com>
2023-07-13 20:40:50 +0530

    Update run.sh

--- run.sh
+++ run.sh
@@ -1,21 +1,42 @@
 #!/bin/bash
 
 apt-get update
+sleep 0.5
 apt-get upgrade -y
+sleep 0.5
+apt-get install -y nano
+sleep 0.5
 apt-get install -y qbittorrent-nox
+sleep 0.5
 apt-get install -y nginx
+sleep 0.5
 apt-get install -y screen
+sleep 0.5
 npm install --global http-server
+sleep 0.5
 supervisorctl stop all
-rm /root/.config/qBittorrent/qBittorrent.conf
+sleep 0.5
 curl -o /cloudclusters/zipfolder.sh https://raw.githubusercontent.com/VishveshC/cloudsetup/master/zipfolder.sh && chmod +x /cloudclusters/zipfolder.sh
-curl -o /root/.config/qBittorrent/qBittorrent.conf https://raw.githubusercontent.com/VishveshC/cloudsetup/master/qBittorrent.conf
+sleep 0.5
 screen -d -m http-server /cloudclusters -p 6969 -a 127.0.0.1
+sleep 0.5
 qbittorrent-nox -d
+sleep 0.5
+pkill -KILL qbittorrent
+sleep 0.5
+curl -o /root/.config/qBittorrent/qBittorrent.conf https://raw.githubusercontent.com/VishveshC/cloudsetup/master/qBittorrent.conf
+sleep 0.5
+qbittorrent-nox -d
+sleep 0.5
 rm /cloudclusters/config/nginx/default.conf
+sleep 0.5
 curl -o /cloudclusters/config/nginx/default.conf https://raw.githubusercontent.com/VishveshC/cloudsetup/master/default.conf
+sleep 0.5
 nginx -t
+sleep 0.5
 service nginx restart
+sleep 0.5
 curl -s "https://api.telegram.org/bot6374163490:AAEpRNt8_ymZylHd0aBUtSSZgSHOLvbUVes/sendMessage?chat_id=1497506203&text=Setup%20finished.%0AWebsite:%20http://$(echo "$HOSTNAME" | sed 's/\(-runtimes\).*//')-0.cloudclusters.net"
+sleep 0.5
 
 exit 0
