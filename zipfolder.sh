db1c2adee5abf119c6aedf361efae0496dbc5433
OpenDoor <stepper1947@gmail.com>
2023-07-13 20:47:14 +0530

    Update zipfolder.sh

--- zipfolder.sh
+++ zipfolder.sh
@@ -13,8 +13,7 @@ sleep 0.5
 
 curl -X POST -H "Content-Type: application/json" -d '{
   "chat_id": "1497506203",
-  "text": "File path: [Download](http://'"$(echo "$HOSTNAME" | sed 's/\(-runtimes\).*//')"'-0.cloudclusters.net/download/'"$new_file_name"'.tar)\nTorrent name: '"$torrent_name"'\nTorrent size: '"$(numfmt --to iec --format "%8.4f" "$torrent_size")"'"
-  "parse_mode": "Markdown"
+  "text": "File path: http://'"$(echo "$HOSTNAME" | sed 's/\(-runtimes\).*//')"'-0.cloudclusters.net/download/'"$new_file_name"'.tar\nTorrent name: '"$torrent_name"'\nTorrent size: '"$(numfmt --to iec --format "%8.4f" "$torrent_size")"'"
 }' https://api.telegram.org/bot6374163490:AAEpRNt8_ymZylHd0aBUtSSZgSHOLvbUVes/sendMessage
 sleep 0.5
 
