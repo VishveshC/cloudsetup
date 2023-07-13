187f182035416c97cdfe334103eb77a6207e1ecd
OpenDoor <stepper1947@gmail.com>
2023-07-13 15:24:29 +0530

    Update zipfolder.sh

--- zipfolder.sh
+++ zipfolder.sh
@@ -1,59 +1,27 @@
 #!/bin/bash
 
-# Telegram bot token and chat ID
-bot_token="6374163490:AAEpRNt8_ymZylHd0aBUtSSZgSHOLvbUVes"
-chat_id="1497506203"
+# Get the torrent name, file path, root path, and save path from qBittorrent.
+torrent_name="$1"
+file_path="$2"
+torrent_size="$3"
 
-# Set the download folder path
-download_folder="/cloudclusters/download"
+# Replace all the spaces in the filename with underscores.
+new_file_name=$(echo "$torrent_name" | tr " " "_")
 
-# Get the current directory
-current_dir="%F"
+# Tar the folder and save it with the new name.
+tar -cf "$new_file_name.tar" "$file_path"
 
-# Extract the base folder name
-base_folder="$(basename "$current_dir")"
+# Move the new tar file to the download directory.
+mv "$new_file_name.tar" /cloudclusters/download
 
-# Replace spaces with underscores in the base folder name
-new_base_folder="${base_folder// /_}"
+# Delete the original folder.
+rm -rf "$file_path"
 
-# Create the tar file path
-tar_file_path="$download_folder/$new_base_folder.tar"
+# Curl the file path, torrent name, and torrent size to the telegram API.
+curl -X POST -H "Content-Type: application/json" -d '{
+  "chat_id": "1497506203",
+  "text": "File path: /cloudclusters/download/$new_file_name.tar\nTorrent name: $torrent_name\nTorrent size: $torrent_size"
+}' https://api.telegram.org/bot6374163490:AAEpRNt8_ymZylHd0aBUtSSZgSHOLvbUVes/sendMessage
 
-# Create a tar archive without compression
-tar -cf "$tar_file_path" -C "$current_dir" .
-
-# Print out the file path
-echo "Tar archive created: $tar_file_path"
-
-# Remove the original directory
-rm -r "$current_dir"
-
-# Print message after deletion
-echo "Original folder deleted: $current_dir"
-
-# Path to the tar file
-tar_file="$tar_file_path"
-
-# Extract file name without path
-file_name=$(basename "$tar_file")
-
-# Calculate file size in bytes
-file_size=$(stat -c%s "$tar_file")
-
-# Calculate file size in gigabytes with one decimal point precision
-file_size_gb=$(awk -v size="$file_size" 'BEGIN{ printf "%.1f", size/1024/1024/1024 }')
-
-# Category
-category="%L"
-
-# Generate the automatic download link
-download_link="http://$(echo "$HOSTNAME" | sed 's/\(-runtimes\).*//')-0.cloudclusters.net/download/$file_name"
-
-# Create the message with properties
-message="File Name: $file_name
-File Size: $file_size_gb GB
-Category: $category
-Download: $download_link"
-
-# Send the message using Telegram bot API
-curl -s -X POST "https://api.telegram.org/bot$bot_token/sendMessage" -d "chat_id=$chat_id" -d "text=$message"
+# Exit the script.
+exit 0
